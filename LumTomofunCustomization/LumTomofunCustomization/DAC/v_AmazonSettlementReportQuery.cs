2871086cd57ff583629499e685a3aa147e22507f
WayneLin <wayne.lin@lumina-tech.com>
2022-09-13 17:26:31 +0800

    1.調整Amazon Settlement report sql view

--- LumTomofunCustomization/LumTomofunCustomization/DAC/v_AmazonSettlementReportQuery.cs
+++ LumTomofunCustomization/LumTomofunCustomization/DAC/v_AmazonSettlementReportQuery.cs
@@ -8,28 +8,28 @@ namespace LUMTomofunCustomization
     public class v_AmazonSettlementReportQuery : IBqlTable
     {
         #region SettlementID
-        [PXDBString(15, IsUnicode = true, InputMask = "", IsKey = true)]
+        [PXDBString(15, IsUnicode = true, InputMask = "")]
         [PXUIField(DisplayName = "Settlement ID")]
         public virtual string SettlementID { get; set; }
         public abstract class settlementID : PX.Data.BQL.BqlString.Field<settlementID> { }
         #endregion
 
         #region PostedDate
-        [PXDBDate(IsKey = true)]
+        [PXDBDate()]
         [PXUIField(DisplayName = "Posted Date")]
         public virtual DateTime? PostedDate { get; set; }
         public abstract class postedDate : PX.Data.BQL.BqlDateTime.Field<postedDate> { }
         #endregion
 
         #region OrderID
-        [PXDBString(100, IsUnicode = true, InputMask = "", IsKey = true)]
+        [PXDBString(100, IsUnicode = true, InputMask = "")]
         [PXUIField(DisplayName = "Order ID")]
         public virtual string OrderID { get; set; }
         public abstract class orderID : PX.Data.BQL.BqlString.Field<orderID> { }
         #endregion
 
         #region TransactionType
-        [PXDBString(20, IsUnicode = true, InputMask = "", IsKey = true)]
+        [PXDBString(20, IsUnicode = true, InputMask = "")]
         [PXUIField(DisplayName = "Transaction Type")]
         public virtual string TransactionType { get; set; }
         public abstract class transactionType : PX.Data.BQL.BqlString.Field<transactionType> { }
@@ -40,6 +40,13 @@ namespace LUMTomofunCustomization
         [PXUIField(DisplayName = "Amount")]
         public virtual Decimal? Amount { get; set; }
         public abstract class amount : PX.Data.BQL.BqlDecimal.Field<amount> { }
+        #endregion
+
+        #region AmountDescriptionSummary
+        [PXDBString(1000, IsUnicode = true)]
+        [PXUIField(DisplayName = "Amount Description Summary")]
+        public virtual string AmountDescriptionSummary { get; set; }
+        public abstract class amountDescriptionSummary : PX.Data.BQL.BqlString.Field<amountDescriptionSummary> { }
         #endregion
 
         #region ErrorMessage
